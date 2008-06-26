7c1a0ba9ae21e02eb5bd17a0a5006dbb51e5bb51
יובל קוג'מן (Yuval Kogman) <nothingmuch@woobling.org>
2008-06-26 03:32:11 +0000

    Import of NUFFIN/Crypt-Util-0.08 from CPAN.
    gitpan-cpan-distribution: Crypt-Util
    gitpan-cpan-version:      0.08
    gitpan-cpan-path:         NUFFIN/Crypt-Util-0.08.tar.gz
    gitpan-cpan-author:       NUFFIN
    gitpan-cpan-maturity:     released

--- lib/Crypt/Util.pm
+++ lib/Crypt/Util.pm
@@ -1,9 +1,9 @@
 #!/usr/bin/perl
 
 package Crypt::Util;
-use Moose;
+use Squirrel;
 
-our $VERSION = "0.07";
+our $VERSION = "0.08";
 
 use Digest;
 use Digest::MoreFallbacks;
@@ -142,7 +142,7 @@ has disable_fallback => (
 	is  => "rw",
 );
 
-__PACKAGE__->meta->make_immutable;
+__PACKAGE__->meta->make_immutable if __PACKAGE__->meta->can("make_immutable");
 
 {
 	my %fallback_caches;
@@ -794,7 +794,7 @@ sub authenticated_encrypt_string {
 		#my $header = $params{header};
 		#$header = '' unless defined $header;
 
-		return pack("n/a a*", $nonce, $self->encrypt_string( %params, nonce => $nonce ) );
+		return pack("n/a* a*", $nonce, $self->encrypt_string( %params, nonce => $nonce ) );
 	} else {
 		croak "To use encrypted tamper resistent strings an authenticated encryption mode such as EAX must be selected";
 	}
@@ -806,7 +806,7 @@ sub authenticated_decrypt_string {
 	if ( $self->_authenticated_mode(\%params) ) {
 		$self->_process_params( \%params, qw(string) );
 
-		my ( $nonce, $string ) = unpack("n/a a*", $params{string});
+		my ( $nonce, $string ) = unpack("n/a* a*", $params{string});
 
 		return $self->decrypt_string(
 			fatal => 1,
